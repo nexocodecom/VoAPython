087748abf02696517f953183f000b8617e2ec39e
Krzysztof Suwada <krzysztof.suwada@gmail.com>
2025-01-14 09:41:14 +0100

    web app v1

--- voa/copula.py
+++ voa/copula.py
@@ -150,7 +150,8 @@ def create_Q_plot(X: List[float], Y: List[float], k_plot_grid: int = 100, MC: in
         x=plot_points['x'],
         y=plot_points['y'],
         contours=dict(
-            coloring='jet'
+            coloring='heatmap',
+            color_continuous_scale='jet'
         )
     )
 
